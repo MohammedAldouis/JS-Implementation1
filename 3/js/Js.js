77c63682470a31d866c0b24900169f68ff01e117
MohammedAldouis <mhmdaldouis08@gmail.com>
2021-07-31 11:22:00 +0300

    js

---
+++ 3/js/Js.js
@@ -0,0 +1,20 @@
+function yesover(){
+   
+    document.getElementById('yes').innerHTML = "مرحبا بك يا اوتاكو";
+}
+
+function yesout(){
+   
+    document.getElementById('yes').innerHTML = "نعم";
+}
+
+function noover(){
+   
+    document.getElementById('no').innerHTML = "هذا الموقع لا يصلح لك"
+}
+
+function noout(){
+   
+    document.getElementById('no').innerHTML = "لا";
+}
+
---
+++ 4/js/Js.js
@@ -0,0 +1,12 @@
+function over(){
+    document.getElementById('one').innerHTML = "استمر بالضغط على الفأرة..."
+}
+function down(){
+    document.getElementById('one').innerHTML = "انا محمد الدعيس "
+}
+function up(){
+    document.getElementById('one').innerHTML = "انا احب الانمي  "
+}
+function out(){
+    document.getElementById('one').innerHTML = "وداعا"
+}
