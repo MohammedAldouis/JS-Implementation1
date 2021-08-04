b7d85f55bfab9fd3148f3ea43dbb8ceaf7774d05
MohammedAldouis <85803945+MohammedAldouis@users.noreply.github.com>
2021-08-04 10:34:22 +0300

    js

---
+++ 7/js/Js.js
@@ -0,0 +1,4 @@
+var p =document.querySelectorAll('p');
+for(i=0; i<p.length; i++){
+    p[i].style.border = '1px solid black'
+}
---
+++ 7/js/script.js
@@ -0,0 +1,6 @@
+var pa = document.createElement('p');
+pa.innerHTML = ('هذه المقاله عن العلم');
+pa.style.color=('red')
+pa.style.fontSize=('50px')
+
+document.body.appendChild(pa)
