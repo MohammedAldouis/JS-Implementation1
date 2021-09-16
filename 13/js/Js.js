3294a70e1b9a751a961ab568acd84f796c53d69e
MohammedAldouis <85803945+MohammedAldouis@users.noreply.github.com>
2021-09-16 11:28:59 +0300

    js

---
+++ 13/js/Js.js
@@ -0,0 +1,13 @@
+let on=prompt();
+let tw=prompt();
+
+function myname(y){
+    document.write(y);
+
+}
+
+myname(on);
+
+
+
+myname(tw);
---
+++ 14/js/js.js
@@ -0,0 +1,8 @@
+var x=5;
+var y=2;
+document.write(y%y);
+document.write(x%y);
+document.write(y%y);
+document.write(x%y);
+document.write(y%y);
+document.write(x%y);
